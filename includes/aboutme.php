6049703593aa3f85eabed3c7ea62bf30c38893c5
Amir Mokhtar Ali <amirmokhtar@g.harvard.edu>
2017-02-08 17:47:04 +0200

    added the local project files

---
+++ includes/aboutme.php
@@ -0,0 +1,22 @@
+<div id="about">
+    <h2>A little About Me....</h2>
+    <p>
+    <h3>Where am I from</h3>
+    I was born in Heliopolis, Cairo, Egypt in the late 70's.<br>
+    I lived in Boston USA, during the 90's's.<br>
+    Currently living Giza, Cairo Egypt.
+    <br>
+    <h3>My Education</h3>
+    In early 90's, I completed my International General Certificate <br>
+    of Secondary Education, by Cambridge University International Exams, England.
+    <br>
+    And by the late 90's, I acquired a bachelor of science, <br>
+    in Electrical Computer Engineering from Northeastern University in Boston, Massachusetts, USA.
+    <br>
+    <h3>My Work Experience and why I am taking this class</h3>
+    I worked As an Engineer, Financier, Marketer, Business development, and a Teacher.<br>
+    I am a retired Business development Engineer and currently developing an algae farm.<br><br>
+    I am currently enrolled in the Mathematics for teaching masters program at HES.<br>
+    This class is my last elective in the program.<br>
+    I expect to complete my degree by next summer, God Willing.<br>
+    </P>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+Amir Mokhatr Ali
+HUID 70922584
+amirmokhtar@g.harvard.edu
+CSCI E-15
+Assignment One
+-->
+<!doctype html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Harvard CSCI E15 Assignment 1</title>
+        <?php include("includes/htmlhead.php"); ?>   
+    </head>
+    <body>
+        <?php include("includes/header.php"); ?>
+        <?php include("includes/myimage.php"); ?>
+        <hr>
+        <?php include("includes/aboutme.php"); ?>
+        <hr>
+        <?php include("includes/quote.php"); ?>
+        <div>
+            <footer>
+                <?php include("includes/footer.php"); ?>
+            </footer>
+        </div>
+
+    </body>
+</html>
