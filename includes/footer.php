32b3d7e5c018648bb83e08919bff00b04db141d1
Amir Mokhtar Ali <amirmokhtar@g.harvard.edu>
2017-02-08 19:26:43 +0200

    trying to fix broken footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,5 +3,5 @@
     This web site was coded by: 
     <a href="mailto:amokhtar@g.harvard.edu?Subject=Hello%20again">Eng. Amir Mokhtar</a>. &copy; Copyright 2017.
     
-    <img src="images/harvard-dce-shield.png" alt="hes" style="float:right;"
+    <img src="images/harvard-dce-shield.png" alt="hes" style="float:right;">
 </div>
